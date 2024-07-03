ce8edf093024146a7d0174a942727c3ef81e6380
Revanth-144 <157783345+Revanth-144@users.noreply.github.com>
2024-07-03 12:09:09 +0530

    Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -1,1 +1,19 @@
+import numpy as np
+# Create a 2D Numpy array of size 1x3 with elements of your choice
+arr1 = np.array([[1, 2, 3]])
 
+# Create a Numpy array of length 50 with zeroes as its elements
+arr2 = np.zeros(50, dtype=np.int64)
+
+#Create a Numpy array of length 3x2 with elements of your choice
+arr3 = np.array([[1, 2], [3, 4], [5, 6]])
+
+arr4=np.dot(arr1,arr3)
+
+#Change 5th element of arr2 to a different number
+arr2[4]=10
+
+if np.shape(arr4)==(1,2) and arr2[4]!=0:
+  print("Passed")
+else:
+  print("Fail")
